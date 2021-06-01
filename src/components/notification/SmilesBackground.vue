210c5fd0ad0922ae84c0c2e183f6c87100dd843a
VlsnkVld <menzlags@gmail.com>
2021-06-01 22:52:51 +0300

    small animation fix;

--- src/components/notification/SmilesBackground.vue
+++ src/components/notification/SmilesBackground.vue
@@ -49,93 +49,68 @@ export default {
   }
   :nth-child(1) {
     left: 95%;
-    animation: down 4s linear 3;
+    animation: down 4s linear 2;
     animation-delay: 1.5s;
   }
   :nth-child(2) {
     left: 80%;
-    animation: down 6.5s linear 3;
+    animation: down 6.5s linear 2;
     animation-delay: 0.2s;
   }
   :nth-child(3) {
     left: 78%;
-    animation: down 4s linear 3;
+    animation: down 4s linear 2;
     animation-delay: 1s;
   }
   :nth-child(4) {
     left: 60%;
-    animation: down 2s linear 3;
+    animation: down 2s linear 2;
     animation-delay: 2.3s;
   }
   :nth-child(5) {
     left: 54%;
-    animation: down 3s linear 3;
+    animation: down 3s linear 2;
     animation-delay: 1.7s;
   }
   :nth-child(6) {
     left: 49%;
-    animation: down 5s linear 3;
+    animation: down 5s linear 2;
     animation-delay: 0.5s;
   }
   :nth-child(7) {
     left: 42%;
-    animation: down 2.5s linear 3;
+    animation: down 2.5s linear 2;
     animation-delay: 1.1s;
   }
   :nth-child(8) {
     left: 33%;
-    animation: down 3.7s linear 3;
+    animation: down 3.7s linear 2;
     animation-delay: 0.7s;
   }
   :nth-child(9) {
     left: 25%;
-    animation: down 4.7s linear 3;
+    animation: down 4.7s linear 2;
     animation-delay: 0.2s;
   }
   :nth-child(10) {
     left: 18%;
-    animation: down 2.7s linear 3;
+    animation: down 2.7s linear 2;
     animation-delay: 1s;
   }
   :nth-child(11) {
     left: 12%;
-    animation: down 5.3s linear 3;
+    animation: down 5.3s linear 2;
     animation-delay: 0.5s;
   }
   :nth-child(12) {
     left: 7%;
-    animation: down 2.8s linear 3;
+    animation: down 2.8s linear 2;
     animation-delay: 0.7s;
   }
   :nth-child(13) {
     left: 3%;
-    animation: down 3.2s linear 3;
+    animation: down 3.2s linear 2;
     animation-delay: 1s;
-  }
-}
-</style>
-
-<style lang="scss">
-@keyframes down {
-  0% {
-    top: -20%;
-    transform: translateX(15px) rotate(0deg);
-  }
-  20% {
-    transform: translateX(-15px) rotate(45deg);
-  }
-  40% {
-    transform: translateX(-7px) rotate(90deg);
-  }
-  60% {
-    transform: translateX(0) rotate(125deg);
-  }
-  80% {
-    transform: translateX(5px) rotate(180deg);
-  }
-  100% {
-    top: 110%;
-    transform: translateX(15px) rotate(225deg);
   }
 }
 </style>
